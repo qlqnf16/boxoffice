0240a1bdb8426345e1ff1a6a9454db9c12256ca1
Jeong Min Lee <jeongminlee@Jeongs-MacBook-Air.local>
2019-05-02 16:00:59 +0900

    준비

--- src/App.js
+++ src/App.js
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
-function App() {
+import Home from './routes/Home';
+import MovieDetail from './routes/MovieDetail';
+
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Router>
+      <div>
+        <Switch>
+          <Route exact path='/' component={Home} />
+          <Route path="/detail" component={MovieDetail} />
+        </Switch>
+      </div>
+    </Router>
+  )
 }
 
 export default App;
---
+++ src/routes/MovieDetail.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class MovieDetail extends React.Component {
+    render() {
+        return (
+            <h1>This is detail</h1>
+        )
+    }
+}
+
+export default MovieDetail
